9b67e3dbf1fb834e6d0c068e482594a60679d7c7
Jonathan Ong <jonathanrichardong@gmail.com>
2016-01-03 18:10:08 -0800

    docs++ & tests++

--- test/index.js
+++ test/index.js
@@ -20,206 +20,218 @@ before(() => {
 })
 
 describe('Redis Cache Decorator', () => {
-  it('should not allow concurrent execution of an asynchronous function', () => {
-    let called = 0
-    const fn = decorator({
-      namespace: createNamespace()
-    })(val => {
-      return wait(10).then(() => {
-        called++
-        return val + 1
+  describe('Concurrency', () => {
+    it('should not allow concurrent execution of an asynchronous function', () => {
+      let called = 0
+      const fn = decorator({
+        namespace: createNamespace()
+      })(val => {
+        return wait(10).then(() => {
+          called++
+          return val + 1
+        })
+      })
+
+      return Promise.all([
+        fn(1),
+        fn(1)
+      ]).then(results => {
+        // equal results
+        assert.deepEqual(results, [2, 2])
+        // only called once
+        assert.equal(called, 1)
       })
     })
 
-    return Promise.all([
-      fn(1),
-      fn(1)
-    ]).then(results => {
-      // equal results
-      assert.deepEqual(results, [2, 2])
-      // only called once
-      assert.equal(called, 1)
-    })
-  })
-
-  it('should not allow concurrent execution of a synchronous function', () => {
-    let called = 0
-    const rand = Math.random()
-    const fn = decorator({
-      namespace: createNamespace()
-    })(val => {
-      called++
-      return val + rand
-    })
-
-    return Promise.all([
-      fn(1),
-      fn(1)
-    ]).then(results => {
-      // equal results
-      assert.deepEqual(results, [1 + rand, 1 + rand])
-      // only called once
-      assert.equal(called, 1)
-    })
-  })
-
-  it('should cache results', () => {
-    let called = 0
-
-    const fn = decorator({
-      namespace: createNamespace()
-    })(val => {
-      return wait(10).then(() => {
+    it('should not allow concurrent execution of a synchronous function', () => {
+      let called = 0
+      const rand = Math.random()
+      const fn = decorator({
+        namespace: createNamespace()
+      })(val => {
         called++
-        return val + 1
+        return val + rand
       })
-    })
 
-    return fn(1).then(val => {
-      assert.equal(val, 2)
-      return fn(1).then(val => {
-        assert.equal(val, 2)
+      return Promise.all([
+        fn(1),
+        fn(1)
+      ]).then(results => {
+        // equal results
+        assert.deepEqual(results, [1 + rand, 1 + rand])
+        // only called once
         assert.equal(called, 1)
       })
     })
   })
 
-  it('should return the same error w/ asynchronous functions', () => {
-    let called = 0
+  describe('Caching', () => {
+    it('should cache results', () => {
+      let called = 0
 
-    const fn = decorator({
-      namespace: createNamespace()
-    })(val => {
-      return wait(100).then(() => {
-        called++
+      const fn = decorator({
+        namespace: createNamespace()
+      })(val => {
+        return wait(10).then(() => {
+          called++
+          return val + 1
+        })
+      })
+
+      return fn(1).then(val => {
+        assert.equal(val, 2)
+        return fn(1).then(val => {
+          assert.equal(val, 2)
+          assert.equal(called, 1)
+        })
+      })
+    })
+
+    describe('ttl=0', () => {
+      it('should not cache w/ ttl=0', () => {
+        let called = 0
+        const fn = decorator({
+          namespace: createNamespace(),
+          ttl: 0,
+        })(val => {
+          called++
+          return val
+        })
+
+        return fn(1).then(val => {
+          assert.equal(val, 1)
+          return fn(1)
+        }).then(val => {
+          assert.equal(val, 1)
+          assert.equal(called, 2)
+        })
+      })
+    })
+  })
+
+  describe('Error Handling', () => {
+    it('should return the same error w/ asynchronous functions', () => {
+      let called = 0
+
+      const fn = decorator({
+        namespace: createNamespace()
+      })(val => {
+        return wait(100).then(() => {
+          called++
+          throw new Error('boom')
+        })
+      })
+
+      return Promise.all([
+        fn(1).then(() => {
+          throw new Error('nope')
+        }).catch(err => {
+          assert.equal(err.message, 'boom')
+        }),
+        fn(1).then(() => {
+          throw new Error('nope')
+        }).catch(err => {
+          assert.equal(err.message, 'boom')
+        })
+      ]).then(() => {
+        assert.equal(called, 1)
+      })
+    })
+
+    it('should return the same error w/ synchronous functions', () => {
+      // difference is that we don't care if the function is called multiple times
+      // when the function does not take a lot of time
+      const fn = decorator({
+        namespace: createNamespace()
+      })(val => {
         throw new Error('boom')
       })
-    })
 
-    return Promise.all([
-      fn(1).then(() => {
-        throw new Error('nope')
-      }).catch(err => {
-        assert.equal(err.message, 'boom')
-      }),
-      fn(1).then(() => {
-        throw new Error('nope')
-      }).catch(err => {
-        assert.equal(err.message, 'boom')
+      return Promise.all([
+        fn(1).then(() => {
+          throw new Error('nope')
+        }).catch(err => {
+          assert.equal(err.message, 'boom')
+        }),
+        fn(1).then(() => {
+          throw new Error('nope')
+        }).catch(err => {
+          assert.equal(err.message, 'boom')
+        })
+      ])
+    })
+  })
+
+  describe('Timeouts', () => {
+    it('should support timeouts from the listener', () => {
+      let called = 0
+      const fn = decorator({
+        namespace: createNamespace(),
+        timeout: 200,
+      })(val => {
+        called++
+        return wait(1000).then(() => val)
       })
-    ]).then(() => {
-      assert.equal(called, 1)
-    })
-  })
 
-  it('should return the same error w/ synchronous functions', () => {
-    // difference is that we don't care if the function is called multiple times
-    // when the function does not take a lot of time
-    const fn = decorator({
-      namespace: createNamespace()
-    })(val => {
-      throw new Error('boom')
-    })
+      // call it
+      fn(true).catch(noop)
 
-    return Promise.all([
-      fn(1).then(() => {
-        throw new Error('nope')
+      // wait for itt o SETNX
+      return wait(100).then(() => {
+        // call it again
+        return fn(true)
+      }).then(() => {
+        throw new Error('boom')
       }).catch(err => {
-        assert.equal(err.message, 'boom')
-      }),
-      fn(1).then(() => {
-        throw new Error('nope')
-      }).catch(err => {
-        assert.equal(err.message, 'boom')
+        assert.equal(called, 1)
+        assert.equal(err.code, 'RCDTIMEDOUT')
       })
-    ])
-  })
-
-  it('should not cache w/ ttl=0', () => {
-    let called = 0
-    const fn = decorator({
-      namespace: createNamespace(),
-      ttl: 0,
-    })(val => {
-      called++
-      return val
     })
 
-    return fn(1).then(val => {
-      assert.equal(val, 1)
-      return fn(1)
-    }).then(val => {
-      assert.equal(val, 1)
-      assert.equal(called, 2)
+    it('should support timeouts from the function', () => {
+      const fn = decorator({
+        namespace: createNamespace(),
+        timeout: 1,
+      })(val => {
+        return wait(1000).then(() => val)
+      })
+
+      return fn(true).then(() => {
+        throw new Error('boom')
+      }).catch(err => {
+        assert.equal(err.code, 'RCDTIMEDOUT')
+      })
     })
   })
 
-  it('should support timeouts from the listener', () => {
-    let called = 0
-    const fn = decorator({
-      namespace: createNamespace(),
-      timeout: 200,
-    })(val => {
-      called++
-      return wait(1000).then(() => val)
-    })
+  describe('Disabling', () => {
+    it('should work w/ disabled=true', () => {
+      let called = 0
+      const fn = require('..')({
+        client,
+        subscriber,
+        disabled: true,
+      })({
+        namespace: createNamespace(),
+      })(val => {
+        called++
+        return wait(1).then(() => val + 1)
+      })
 
-    // call it
-    fn(true).catch(noop)
-
-    // wait for itt o SETNX
-    return wait(100).then(() => {
-      // call it again
-      return fn(true)
-    }).then(() => {
-      throw new Error('boom')
-    }).catch(err => {
-      assert.equal(called, 1)
-      assert.equal(err.code, 'RCDTIMEDOUT')
-    })
-  })
-
-  it('should support timeouts from the function', () => {
-    const fn = decorator({
-      namespace: createNamespace(),
-      timeout: 1,
-    })(val => {
-      return wait(1000).then(() => val)
-    })
-
-    return fn(true).then(() => {
-      throw new Error('boom')
-    }).catch(err => {
-      assert.equal(err.code, 'RCDTIMEDOUT')
+      return Promise.all([
+        fn(1),
+        fn(1),
+        fn(1),
+      ]).then(results => {
+        for (const result of results) {
+          assert.equal(result, 2)
+        }
+        assert.equal(called, 3)
+      })
     })
   })
 
-  it('should work w/ disabled=true', () => {
-    let called = 0
-    const fn = require('..')({
-      client,
-      subscriber,
-      disabled: true,
-    })({
-      namespace: createNamespace(),
-    })(val => {
-      called++
-      return wait(1).then(() => val + 1)
-    })
-
-    return Promise.all([
-      fn(1),
-      fn(1),
-      fn(1),
-    ]).then(results => {
-      for (const result of results) {
-        assert.equal(result, 2)
-      }
-      assert.equal(called, 3)
-    })
-  })
-
-  describe('validations', () => {
+  describe('Validations', () => {
     describe('constructor options', () => {
       it('should only support ttl=string|number', () => {
         assert.throws(require('..')({
